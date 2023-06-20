c3ed734263a5721980724c561c451ac1718b9f5c
Achmad Rijalu Arianindita <achmad.rijalu@gmail.com>
2023-06-20 09:56:02 +0700

    Initial Commit
    Initial Commit

---
+++ Co-Space/Co_SpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Co_SpaceApp.swift
+//  Co-Space
+//
+//  Created by Achmad Rijalu on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Co_SpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
