754d43d69097974e402f1911f9e209529d370fbf
Achmad Rijalu Arianindita <achmad.rijalu@gmail.com>
2023-06-21 21:35:33 +0700

    Main Menu Slider Navigate new Views
    Main Menu Slider Navigate new Views

---
+++ Co-Space/Views/GuiderView.swift
@@ -0,0 +1,22 @@
+//
+//  GuiderView.swift
+//  Co-Space
+//
+//  Created by Achmad Rijalu on 20/06/23.
+//
+
+import SwiftUI
+import SpriteKit
+
+struct GuiderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GuiderView_Previews: PreviewProvider {
+    static var previews: some View {
+        GuiderView().previewInterfaceOrientation(.landscapeRight)
+    }
+}
+
