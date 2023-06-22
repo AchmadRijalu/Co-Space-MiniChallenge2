89b550f18f8becf160977e08df4e96e70bb4d817
Achmad Rijalu Arianindita <achmad.rijalu@gmail.com>
2023-06-22 09:14:02 +0700

    initiate Guider screen
    initiate Guider screen

---
+++ Co-Space/Scenes/GuiderGameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GuiderGameScene.swift
+//  Co-Space
+//
+//  Created by Achmad Rijalu on 22/06/23.
+//
+
+import SwiftUI
+import SpriteKit
+
+class GuiderGameScene: SKScene{
+    
+    
+    
+}
--- Co-Space/Views/GuiderView.swift
+++ Co-Space/Views/GuiderView.swift
@@ -9,8 +9,22 @@ import SwiftUI
 import SpriteKit
 
 struct GuiderView: View {
+    
+    let screenWidth = UIScreen.main.bounds.width
+    let screenHeight = UIScreen.main.bounds.height
+    
+    //this section is defining the entire gamescreen
+    var scene:SKScene{
+        let scene = GuiderGameScene()
+        scene.size = CGSize(width: screenWidth, height: screenHeight)
+        scene.scaleMode = .fill
+        scene.backgroundColor = SKColor(named: "BackgroundColor") ?? .blue
+        return scene
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            SpriteView(scene: scene)
+        }.ignoresSafeArea()
     }
 }
 
