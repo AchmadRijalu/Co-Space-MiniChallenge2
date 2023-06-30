0f8e65ad6a2cccde90181366d8acd59602416445
NathaliaMinoque <75228926+NathaliaMinoque@users.noreply.github.com>
2023-06-30 17:23:11 +0700

    Clickable Shop and Storage

--- Co-Space/Scenes/InventoryGameScene.swift
+++ Co-Space/Scenes/InventoryGameScene.swift
@@ -19,16 +19,16 @@ class InventoryGameScene: SKScene {
     var inventoryTriangleCard: SKNode?
     var inventoryCircleCard: SKNode?
     var inventoryRectangleCard: SKNode?
-//    var inventoryStorageDoorLeft: [SKNode] = []
+    var inventoryStorageDoorLeft: [SKNode] = []
     var inventoryStorageDoorRight: [SKNode] = []
-    var inventoryStorageDoorLeft: SKNode?
+//    var inventoryStorageDoorLeft: SKNode?
     
     var isDoorOpen: Bool = false
     
     // MARK: - Buttons
     var inventoryButtonBuy: SKNode?
 //    var inventoryOpenButtonNode: [SKNode] = []
-    var inventoryButtonOpen: SKNode?
+    var inventoryButtonOpen: [SKNode] = []
 //    var counter = 0
     
     override func sceneDidLoad() {
@@ -69,19 +69,50 @@ class InventoryGameScene: SKScene {
         if let inventoryButtonBuyNode = scene?.childNode(withName: "inventory-button-buy") {
             inventoryButtonBuy = inventoryButtonBuyNode
         }
-        if let inventoryStorageDoorLeftNode = scene?.childNode(withName: "inventory-storage-door-left-1") {
-            inventoryStorageDoorLeft = inventoryStorageDoorLeftNode
+        for i in 1...3 {
+            if let inventoryStorageDoorLeftNode = scene?.childNode(withName: "inventory-storage-door-left-\(i)") {
+                inventoryStorageDoorLeft.append(inventoryStorageDoorLeftNode)
+            }
+            if let inventoryStorageDoorRightNode = scene?.childNode(withName: "inventory-storage-door-right-\(i)") {
+                inventoryStorageDoorRight.append(inventoryStorageDoorRightNode)
+            }
+            if let inventoryButtonOpenNode = scene?.childNode(withName: "inventory-button-open-\(i)") {
+                inventoryButtonOpen.append(inventoryButtonOpenNode)
+            }
         }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-            guard let touch = touches.first else { return }
-            let touchLocation = touch.location(in: self)
-            
-            if let node = self.atPoint(touchLocation) as? SKSpriteNode, node.name == "inventory-button-open-1" {
-                changeImage(node: inventoryStorageDoorLeft!, imageName: "inventory-label")
-            }
+        guard let touch = touches.first else { return }
+        let touchLocation = touch.location(in: self)
+        
+        resetItemShop()
+        
+        if let node = self.atPoint(touchLocation) as? SKSpriteNode, node.name == "inventory-button-open-1" {
+            inventoryStorageDoorLeft[0].isHidden = true
+            inventoryStorageDoorRight[0].isHidden = true
         }
+        if let node = self.atPoint(touchLocation) as? SKSpriteNode, node.name == "inventory-button-open-2" {
+            inventoryStorageDoorLeft[1].isHidden = true
+            inventoryStorageDoorRight[1].isHidden = true
+        }
+        if let node = self.atPoint(touchLocation) as? SKSpriteNode, node.name == "inventory-button-open-3" {
+            inventoryStorageDoorLeft[2].isHidden = true
+            inventoryStorageDoorRight[2].isHidden = true
+        }
+        if let node = self.atPoint(touchLocation) as? SKSpriteNode, node.name == "inventory-potion-card-off" {
+            changeImage(node: inventoryPotionCard!, imageName: "inventory-potion-card-on")
+        }
+        if let node = self.atPoint(touchLocation) as? SKSpriteNode, node.name == "inventory-triangle-card-off" {
+            changeImage(node: inventoryTriangleCard!, imageName: "inventory-triangle-card-on")
+        }
+        if let node = self.atPoint(touchLocation) as? SKSpriteNode, node.name == "inventory-circle-card-off" {
+            changeImage(node: inventoryCircleCard!, imageName: "inventory-circle-card-on")
+        }
+        if let node = self.atPoint(touchLocation) as? SKSpriteNode, node.name == "inventory-rectangle-card-off" {
+            changeImage(node: inventoryRectangleCard!, imageName: "inventory-rectangle-card-on")
+        }
+    }
     
     func changeImage(node: SKNode, imageName: String) {
         if let nodeToChange = node as? SKSpriteNode {
@@ -89,11 +120,18 @@ class InventoryGameScene: SKScene {
         }
     }
     
-    func openDoor() {
-        let rotateAction = SKAction.rotate(byAngle: .pi / 2, duration: 1.0)
-        inventoryStorageDoorLeft!.run(rotateAction)
-        isDoorOpen = true
+    func resetItemShop() {
+        changeImage(node: inventoryPotionCard!, imageName: "inventory-potion-card-off")
+        changeImage(node: inventoryTriangleCard!, imageName: "inventory-triangle-card-off")
+        changeImage(node: inventoryCircleCard!, imageName: "inventory-circle-card-off")
+        changeImage(node: inventoryRectangleCard!, imageName: "inventory-rectangle-card-off")
     }
+    
+//    func openDoor() {
+//        let rotateAction = SKAction.rotate(byAngle: .pi / 2, duration: 1.0)
+//        inventoryStorageDoorLeft!.run(rotateAction)
+//        isDoorOpen = true
+//    }
     
 //    override func update(_ currentTime: TimeInterval) {
 //        counter += 1
