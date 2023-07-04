a7e8f61767ab3180281c8d6b13624bab0c5df7fb
NathaliaMinoque <75228926+NathaliaMinoque@users.noreply.github.com>
2023-07-04 10:14:00 +0700

    add frontend

---
+++ Co-Space/Views/ResultView.swift
@@ -0,0 +1,20 @@
+//
+//  ResultView.swift
+//  Co-Space
+//
+//  Created by Nathalia Minoque Kusuma Salma Rasyid Jr. on 04/07/23.
+//
+
+import SwiftUI
+
+struct ResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        ResultView()
+    }
+}
