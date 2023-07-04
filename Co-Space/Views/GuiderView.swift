8a60aad78e90fff659c182e1103461dd1544b686
Neilson Soeratman <neilsonsoeratman@gmail.com>
2023-07-04 11:25:41 +0700

    rapiin guiderview dikit

--- Co-Space/Views/GuiderView.swift
+++ Co-Space/Views/GuiderView.swift
@@ -9,33 +9,19 @@ import SwiftUI
 import SpriteKit
 
 struct GuiderView: View {
-    
     let screenWidth = UIScreen.main.bounds.width
     let screenHeight = UIScreen.main.bounds.height
+    var scene = SKScene(fileNamed: "GuiderGameScene.sks") as! GuideScene
     
-    //this section is defining the entire gamescreen
-
-    var scene = SKScene(fileNamed: "GuiderGameScene.sks")
     var body: some View {
-        
         ZStack{
-            
             VStack{
-                
-                SpriteView(scene: scene!).ignoresSafeArea()
-                  
+                SpriteView(scene: scene).ignoresSafeArea()
             }.ignoresSafeArea().onAppear(){
-                scene?.size = CGSize(width: screenWidth, height: screenHeight)
-                scene?.scaleMode = .fill
-                scene?.backgroundColor = SKColor(named: "DarkPurple") ?? .blue
+                scene.size = CGSize(width: screenWidth, height: screenHeight)
+                scene.scaleMode = .fill
+                scene.backgroundColor = SKColor(named: "DarkPurple") ?? .blue
             }
-//            VStack{
-//                HStack{
-//                    Text("Esc").font(.title3)
-//                    Spacer()
-//                }
-//                Spacer()
-//            }
         }
         .navigationBarBackButtonHidden()
     }
